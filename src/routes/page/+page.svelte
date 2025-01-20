3030367fbcccc55b7809578d84ed8ff5f1df4d59
tjjjia <14137521+tjjjia@users.noreply.github.com>
2025-01-20 12:01:20 +0100

    add page + build instructions

---
+++ src/routes/page/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    /** @type {{ data: import('./$types').PageData }} */
+    let { data } = $props();
+</script>
+
+<h1>Gallery</h1>
+<p>Hello World</p>
+<p>Go back <a href="./">home</a>.</p>
