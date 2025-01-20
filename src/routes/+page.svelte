d9f90164800e34220ef84b890294c7a3bff69dd0
tjjjia <14137521+tjjjia@users.noreply.github.com>
2025-01-20 12:29:02 +0100

    routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+	import { base } from '$app/paths';
+	console.log(base)
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<p>Go to a <a href="/page">page</a>.</p>
+<p>Go to a <a href="page">page</a>.</p>
--- svelte.config.js
+++ svelte.config.js
@@ -11,6 +11,9 @@ const config = {
 		}),
 		paths: {
 			base: process.argv.includes("dev") ? "" : process.env.BASE_PATH
+		},
+		prerender: {
+			entries: ["*"]
 		}
 	}
 };
